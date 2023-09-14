4395897a2a33c712abcdb600a72d510a59cb937e
shijianjun <shijianjun@zhizukj.com>
2023-09-14 19:21:13 +0800

    feat: default package.json is added to the first place

--- examples/bumpx.config.ts
+++ examples/bumpx.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from '../src'
 export default defineConfig({
   recursive: true,
   select: true,
+  tag: false,
+  commit: false
   // files: ['packages/**/package.json']
 })
